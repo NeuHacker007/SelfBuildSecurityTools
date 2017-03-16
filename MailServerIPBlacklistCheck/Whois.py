58531a58e7a043effe14e03aa9ebc754d5c9a8bb
Yifan Zhang <zhang.yifan2@husky.neu.edu>
2017-03-16 15:04:16 -0400

    Refined the functions

--- MailServerIPBlacklistCheck/Whois.py
+++ MailServerIPBlacklistCheck/Whois.py
@@ -3,9 +3,13 @@ import urllib
 import json
 import sys
 import getopt
+import datetime
 
 "Define global variables"
-
+IP = '98.150.108.228'
+defaultAPI = 'https://www.dnswatch.info/blacklist-lookup-progress'
+defaultUserIDLookUpAddr = 'https://www.dnswatch.info/blacklist-lookup'
+isShowProgress = False
 '''
    This function is used to retrieve the json from the
    the Email Server Blacklist API @ https://www.dnswatch.info/dns/rbl-lookup
@@ -17,32 +21,56 @@ def getJsonFromAPI(_completeURL):
 
 def getUserID(_ipaddr):
     "1. compose the URL of getting API userid"
-    useridURL =  'https://www.dnswatch.info/blacklist-lookup' + '?host=' + _ipaddr
+    useridURL =  defaultUserIDLookUpAddr + '?host=' + _ipaddr
     "2.call getJsonFromAPI to get the json string returned from API"
     originJson,apiJson = getJsonFromAPI(useridURL)
     "3. get the userid from the json string"
     userid = apiJson['progressKey']
+
+    if isShowProgress:
+        print "The API user ID has been successfully retrieved from: " + defaultUserIDLookUpAddr
     return userid
 
 def getBlacklistResult(_ipaddr):
     "1.compose new request URL"
-    requestURL = 'https://www.dnswatch.info/blacklist-lookup-progress' + '?uid=' + getUserID(_ipaddr)
+    requestURL =  defaultAPI + '?uid=' + getUserID(_ipaddr)
     "2.call getJsonFromAPI to get the json string from API"
     originalJson, apiJson = getJsonFromAPI(requestURL)
+
+    if  isShowProgress:
+        print "Data has been successfully retrieved from API at address of: " + defaultAPI
+        print "Returned data is: \n" + str(apiJson)
     return apiJson
 
-
-
-def main():
+def main(arguments):
     "use different options to specify parameters"
+    "The getopt parameter with ':' followed means that it requires a value after the option"
+    try:
+        opts,args = getopt.getopt(arguments,"vi:")
+        for option,optarg in opts:
+            if option in ("-v", "--ShowProgress"):
+                global isShowProgress
+                isShowProgress = True;
+            elif option in ("-i", "--TargetIP"):
+                global IP
+                IP = optarg
+    except getopt.GetoptError as e:
+        print "The errors shows as follows: \n" + str(e)
+
+    if isShowProgress:
+        print "Program starts at: " + str(datetime.datetime.now())
+        print "IP address to check is: " + IP
+        print "-----------------------------------------------------------------"
 
     "check the result"
-    blacklistResultJson = getBlacklistResult('216.58.194.46')
-    print blacklistResultJson
+    blacklistResultJson = getBlacklistResult(IP)
+
     "judge whether positiveReults has value or not"
     "You can change this IP to a variable."
     results = blacklistResultJson['positiveResults']
-    print results
+    if isShowProgress:
+        print results
+
     if not results:
         print 'this IP is not blocked!'
     else:
@@ -51,7 +79,7 @@ def main():
 
 "The program entry"
 if __name__ == "__main__":
-    "main(sys.argv[1:])"
-    main()
+    main(sys.argv[1:])
+
 
 
